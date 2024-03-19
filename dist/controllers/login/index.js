559fb05899d9b79d4e50e12434ff1385c4103fce
abbas bandar <abbasbandar@gmail.com>
2024-03-19 16:04:25 +0100

    vvd

--- dist/controllers/login/index.js
+++ dist/controllers/login/index.js
@@ -22,6 +22,15 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -30,18 +39,18 @@ exports.login = void 0;
 const os_1 = __importDefault(require("os"));
 const axios_1 = __importDefault(require("axios"));
 const crypto = __importStar(require("crypto"));
-const login = async () => {
+const login = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const identifier = os_1.default.networkInterfaces()['en0'].find((x) => x.family === 'IPv4').mac || "02:00:00:00:00:10";
         try {
-            const { data: { jwt } } = await axios_1.default.post('https://store.mc-amc.com/app-store/api/auth/local/device/login', {
+            const { data: { jwt } } = yield axios_1.default.post('https://store.mc-amc.com/app-store/api/auth/local/device/login', {
                 identifier
             });
             return jwt;
         }
         catch (_a) {
             const time = Date.now();
-            const { data: { jwt } } = await axios_1.default.post('https://store.mc-amc.com/app-store/api/auth/local/device/register', {
+            const { data: { jwt } } = yield axios_1.default.post('https://store.mc-amc.com/app-store/api/auth/local/device/register', {
                 identifier,
                 model: "Pulse",
                 time,
@@ -53,5 +62,5 @@ const login = async () => {
     catch (error) {
         console.error('Error:', error);
     }
-};
+});
 exports.login = login;
--- dist/controllers/start/index.js
+++ dist/controllers/start/index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -9,23 +18,25 @@ const generateRandomTime_1 = require("./generateRandomTime");
 const login_1 = require("../login");
 const getAllData_1 = require("../getAllData");
 const loadWebPages_1 = require("../loadWebPages");
-async function startLoadingWebPages() {
-    const cronSchedule = (0, generateRandomTime_1.generateRandomTime)();
-    node_cron_1.default.schedule(cronSchedule, async () => {
-        try {
-            const token = await (0, login_1.login)();
-            if (!token) {
-                console.error('Authentication failed');
-                return;
+function startLoadingWebPages() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const cronSchedule = (0, generateRandomTime_1.generateRandomTime)();
+        node_cron_1.default.schedule(cronSchedule, () => __awaiter(this, void 0, void 0, function* () {
+            try {
+                const token = yield (0, login_1.login)();
+                if (!token) {
+                    console.error('Authentication failed');
+                    return;
+                }
+                const { data } = yield (0, getAllData_1.getAllData)(token);
+                for (const webpage of data.websites) {
+                    yield (0, loadWebPages_1.loadWebPage)(webpage);
+                }
             }
-            const { data } = await (0, getAllData_1.getAllData)(token);
-            for (const webpage of data.websites) {
-                await (0, loadWebPages_1.loadWebPage)(webpage);
+            catch (error) {
+                console.error('Error during scheduled task:', error);
             }
-        }
-        catch (error) {
-            console.error('Error during scheduled task:', error);
-        }
+        }));
     });
 }
 exports.startLoadingWebPages = startLoadingWebPages;
